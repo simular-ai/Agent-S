b99c5d598e397cd1bff223df82c2227606bac8ff
sygan <derek.sygan@gmail.com>
2024-10-20 01:23:48 +0800

    Add comments

--- agent_s/query_perplexica.py
+++ agent_s/query_perplexica.py
@@ -2,8 +2,10 @@ import requests
 import json
 
 def query_to_perplexica(query):
+    # Your URL for searching
     url = 'http://localhost:3001/api/search'
     
+    # Your request
     message = {
         "chatModel": {
             "provider": "openai",
@@ -21,15 +23,15 @@ def query_to_perplexica(query):
     }
 
     response = requests.post(url, json=message)
-    # print(response)
+
     if response.status_code == 200:
-        return response.json()['message']
+        return response.json()['message'] 
     elif response.status_code == 400:
         raise ValueError('The request is malformed or missing required fields, such as FocusModel or query')
     else:
         raise ValueError('Internal Server Error')
     
-
+# For test of availability of Perplexica, simply run this script
 if __name__ == "__main__":
     query = "What is Agent S?"
     response = query_to_perplexica(query)
