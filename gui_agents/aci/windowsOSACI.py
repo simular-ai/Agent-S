3c485940b0dcf80bc0c67773db02ea5576d963af
Vincent Tu <58043265+alckasoc@users.noreply.github.com>
2025-01-01 08:49:32 +0000

    move file

--- gui_agents/aci/windowsOSACI.py
+++ gui_agents/aci/windowsOSACI.py
@@ -17,6 +17,7 @@ from ACI import ACI, agent_action
 def _normalize_key(key: str) -> str:
     """Convert 'ctrl' to 'control' for pyautogui compatibility"""
     return 'ctrl' if key == 'control' else key
+    
 def list_apps_in_directories():
     directories_to_search = [
         os.environ.get('PROGRAMFILES', 'C:\\Program Files'),
