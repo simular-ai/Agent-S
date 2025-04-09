d1ecf5b70e14484084a65ebbb398850a9e808d98
alckasoc <tuvincent0106@gmail.com>
2025-04-08 17:39:38 -0700

    fix lib run single

--- osworld_setup/s2/lib_run_single.py
+++ osworld_setup/s2/lib_run_single.py
@@ -12,7 +12,7 @@ def run_single_example(
     agent, env, example, max_steps, instruction, args, example_result_dir, scores
 ):
     runtime_logger = setup_logger(example, example_result_dir)
-    agent.reset(runtime_logger)
+    agent.reset()
     env.reset(task_config=example)
     time.sleep(60)  # Wait for the environment to be ready
     obs = env._get_obs()  # Get the initial observation
