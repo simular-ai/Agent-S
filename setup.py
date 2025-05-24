01c741815fd168feecb2b4962578290253db337d
alckasoc <tuvincent0106@gmail.com>
2025-05-23 19:24:48 -0700

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="gui-agents",
-    version="0.2.4",
+    version="0.2.5",
     description="A library for creating general purpose GUI agents using multimodal LLMs.",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
